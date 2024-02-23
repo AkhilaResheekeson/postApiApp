b79ecadfa4045eb5370eea365c643111bbad3662
Akhila <akhilapr7@gmail.com>
2024-02-23 22:53:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from "react";
 import './App.css';
 
 function App() {
+  const [post,setPost]=useState([]);
+  const [loading,setLoding]=useState(true);
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(Response=>Response.json())
+    .then(data=>{
+      setPost(data);
+      setLoding(false);
+    })
+    .catch(error=>{
+      console.error("Error occurred while fetching...",error);
+    });
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>POSTS</h1>
+      {
+        loading?(
+          <p>Loading...</p>
+        ):(
+          <ul>
+            {post.map(p=>(
+              <li>
+                <h3>User ID: {p.id}</h3>
+                <h4><u>{p.title}</u></h4>
+                <p><i>{p.body}</i></p>
+              </li>
+            ))}
+          </ul>
+        )
+      }
+      
     </div>
   );
 }
